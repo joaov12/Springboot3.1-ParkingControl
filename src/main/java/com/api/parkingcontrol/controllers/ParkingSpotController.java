3084ace7d41e068d81497cc1ab02cd35504e906c
Joao Vitor <jvfs12th@gmail.com>
2023-05-21 14:00:38 -0300

    ParkingSpotController e ParkingSpotDTO

---
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,19 @@
+package com.api.parkingcontrol.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.parkingcontrol.servicies.ParkingSpotService;
+
+@RestController
+@CrossOrigin(originPatterns = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+	@Autowired
+	ParkingSpotService parkingSpotService;
+	
+	
+}
--- src/main/java/com/api/parkingcontrol/servicies/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/servicies/ParkingSpotService.java
@@ -1,5 +1,14 @@
 package com.api.parkingcontrol.servicies;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
 public class ParkingSpotService {
 
+	@Autowired
+	ParkingSpotRepository parkingSpotRepository;
+	
 }
